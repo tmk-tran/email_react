bd460802241e43bbecc2500d9fac81ecccfb5e6b
T Mark <tmk1.tran@gmail.com>
2024-06-12 19:16:39 -0500

    App setup

---
+++ email_react/src/components/Email/EmailTemplate.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function EmailTemplate() {
+  return (
+    <div>
+      <Typography variant="h3">Email Template</Typography>
+    </div>
+  );
+}
